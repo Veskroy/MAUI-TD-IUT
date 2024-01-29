9b55f428235b3091e9c73a233ae354c2e0e2d1fa
lind0013 <thomas.linden1@etudiant.univ-reims.fr>
2024-01-29 09:42:22 +0100

    Création du second projet

---
+++ Taxe/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Taxe
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Taxe/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Taxe
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
