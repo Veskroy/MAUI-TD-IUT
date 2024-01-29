284e456d9c48b47e060a7f3e44f62b0d5d4e653d
lind0013 <thomas.linden1@etudiant.univ-reims.fr>
2024-01-29 09:22:04 +0100

    Sauvegarde des données

--- TP1/App.xaml.cs
+++ TP1/App.xaml.cs
@@ -5,11 +5,35 @@ namespace TP1
     public partial class App : Application
     {
         public static ObservableCollection<string> NumérosAppelés { get; set; } = new ObservableCollection<string>();
+        public const string NUMEROS_APPELES = "numerosAppelés";
+
         public App()
         {
             InitializeComponent();
 
             MainPage = new AppShell();
+        }
+
+        protected override void OnSleep()
+        {
+            string numerosConcatener = string.Join(";", NumérosAppelés);
+
+            Preferences.Set(NUMEROS_APPELES, numerosConcatener);
+        }
+
+        protected override void OnStart()
+        {
+            if (Preferences.ContainsKey(NUMEROS_APPELES))
+            {
+                string numerosConcatener = Preferences.Get(NUMEROS_APPELES, string.Empty);
+
+                string[] numerosArray = numerosConcatener.Split(';');
+
+                foreach (string numero in numerosArray)
+                {
+                    NumérosAppelés.Add(numero);
+                }
+            }
         }
     }
 }
