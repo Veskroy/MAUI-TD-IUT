fb39ae182063f8225bcdf1a85443a5656afa4374
lind0013 <thomas.linden1@etudiant.univ-reims.fr>
2024-03-04 11:43:37 +0100

    calculatrice 33. tests non fonctionnels

---
+++ CalculatriceLibrary/Calculatrice.cs
@@ -0,0 +1,77 @@
+﻿using System.Data;
+
+namespace CalculatriceLibrary
+{
+    public class Calculatrice
+    {
+        public string Opérations { get;  private set; }
+        public double Résultat { get; private set; }
+        private Dictionary<Opération, string> dictionnaire;
+        private bool _opérateur = false;
+        private bool _unique_0 = true;
+        public Calculatrice()
+        {
+            Opérations = "0";
+            Résultat = 0.0;
+        }
+
+        public bool AddDigit(int digit)
+        {
+            if (digit < 0 || digit > 9)
+            {
+                throw new ArgumentOutOfRangeException($"{digit} n'est pas compris  entre 0 et 9 ");
+            }
+            if (digit == 0)
+            {
+                if (_opérateur == true)
+                {
+                    Opérations += "0";
+                    _unique_0 = true;
+                    return true;
+                }
+                else if (_unique_0 == true)
+                {
+                    return false;
+                }
+                else
+                {
+                    Opérations += "0";
+                    _unique_0 = true;
+                    return true;
+                }
+            }
+            if (_unique_0 == true)
+            {
+                Opérations = Opérations.Substring(0, Opérations.Length - 1) + digit.ToString();
+            }
+            else
+            {
+                Opérations += digit.ToString();
+            }
+            _opérateur = false;
+            DataTable dt = new DataTable();
+            Résultat = double.Parse(dt.Compute(Opérations,null).ToString() ?? "0.0");
+            _unique_0 = false;
+            return true;
+        }
+        // void AddOpérateur( ?? opérateur)
+
+        public void Reset()
+        {
+            _opérateur=false;
+            _unique_0 = true;
+            Opérations = "0";
+            Résultat = 0.0;
+        }
+
+      
+    }
+
+    enum Opération
+    {
+        ADDITIONNER = 0,
+        SOUSTRAIRE = 1, 
+        MULTIPLIER = 2, 
+        DIVISER = 3
+    }
+}
---
+++ TestCalculatrice/UnitTestCalculatrice.cs
@@ -0,0 +1,58 @@
+using CalculatriceLibrary;
+
+namespace TestCalculatrice
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public static Random RND = new();
+        [TestMethod]
+        public void TestOperandesThrowArgumentOutOfRange()
+        {
+            Calculatrice calc = new();
+            calc.AddDigit(1);
+            string res = "1";
+            for (int _ = 0; _ < 100; ++_)
+            {
+                int v = RND.Next(-100, 100);
+                if (v >= 0 && v <= 9)
+                {
+                    calc.AddDigit(v);
+                    res += v.ToString();
+                    Assert.AreEqual(res, calc.Opérations);
+                }
+                else
+                {
+                    Assert.ThrowsException<ArgumentOutOfRangeException>(() => { calc.AddDigit(v); });
+                }
+            }
+        }
+        [TestMethod]
+        public void TestOperandes()
+        {
+            // tests aléatoires
+            Calculatrice calc = new();
+            for (int _ = 0; _ < 100; ++_)
+            {
+                calc.Reset();
+                calc.AddDigit(0);
+                Assert.IsTrue(Math.Abs(calc.Résultat) < 1e-5, $"Le résultat ({calc.Résultat}) devrait être nul !");
+                Assert.AreEqual("0", calc.Opérations);
+                calc.AddDigit(0);
+                Assert.IsTrue(Math.Abs(calc.Résultat) < 1e-5, $"Le résultat ({calc.Résultat}) devrait être nul !");
+                Assert.AreEqual("0", calc.Opérations);
+                double res = 0.0;
+                for (int __ = 0; __ < 5; __++)
+                {
+                    int digit = RND.Next(0, 10);
+                    res = res * 10.0 + digit;
+                    calc.AddDigit(digit);
+                    Assert.IsTrue(Math.Abs(calc.Résultat - res) < 1e-5,
+                     $"Le résultat {calc.Résultat} devrait être égal à {res}");
+                    Assert.AreEqual(((int)res).ToString(), calc.Opérations);
+                }
+            }
+        }
+
+    }
+}            
