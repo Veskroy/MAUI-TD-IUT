25fe9d958523e02a4c7126085666db2b63f71ecc
lind0013 <thomas.linden1@etudiant.univ-reims.fr>
2024-03-25 09:31:22 +0100

    création classe VMListCitations

---
+++ Fichier/ViewModel/VMCitation.cs
@@ -0,0 +1,53 @@
+﻿using Fichier.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fichier.ViewModel
+{
+    public class VMCitation
+    {
+        private readonly Citation Citation = new Citation();
+        public string Auteur { 
+            get 
+            {
+                return Citation.Auteur;
+            } 
+            set
+            {
+                if (Citation.Auteur != value)
+                {
+                    Citation.Auteur = value;
+                    Auteur = value;
+                }
+            }
+        }
+        public String Texte {
+            get
+            {
+                return Citation.Texte;
+            }
+            set
+            {
+                if (Citation.Texte != value)
+                {
+                    Citation.Texte = value;
+                    Texte = value;
+                }
+            }
+        }
+
+        public VMCitation()
+        {
+            Auteur = "Albert Einstein";
+            Texte = "Deux choses sont infinies : l'Univers et la bêtise humaine. Mais en ce qui concerne l'Univers, je n'en ai pas encore acquis la certitude absolue.";
+        }
+
+        public VMCitation(Citation Citation)
+        {
+            this.Citation = Citation;
+        }
+    }
+}
---
+++ Fichier/ViewModel/VMListCitations.cs
@@ -0,0 +1,31 @@
+﻿using Fichier.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fichier.ViewModel
+{
+    public class VMListCitations
+    {
+        private readonly ListCitations listCitations = new ListCitations();
+        private ObservableCollection<VMCitation>? vmList = null;
+        public IList<VMCitation> Citations { 
+            get 
+            { 
+                if (vmList == null)
+                {
+                    vmList = new ObservableCollection<VMCitation>();
+                    foreach (var cit in listCitations.Citations)
+                    {
+                        vmList.Add(new VMCitation(cit));
+                    }
+                }
+                return Citations;
+            } 
+            private set { Citations = value; } 
+        }
+    }
+}
